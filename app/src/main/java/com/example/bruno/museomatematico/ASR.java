679a952cb0fd7d244b25106f12a9459568ec88ff
Bruno-sm <bru.1bruno@gmail.com>
2018-10-23 00:39:54 +0200

    Separa el ASR y el TTS en sus respectivas clases

---
+++ app/src/main/java/com/example/bruno/museomatematico/ASR.java
@@ -0,0 +1,101 @@
+package com.example.bruno.museomatematico;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.speech.RecognizerIntent;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.Pair;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.app.Activity.RESULT_OK;
+
+public class ASR {
+    private Activity my_activity;
+    private final static int REQUEST_CODE = 123;
+    private final static String LOGTAG = "ASR";
+    private final static String DEFAULT_LANG_MODEL = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
+    private String languageModel = DEFAULT_LANG_MODEL;
+    // Default values for the language model and maximum number of recognition results
+    // They are shown in the GUI when the app starts, and they are used when the user selection is not valid
+    private final static int DEFAULT_NUMBER_RESULTS = 10;
+    private int numberRecoResults = DEFAULT_NUMBER_RESULTS;
+    private final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 22;
+
+
+    public ASR(MainActivity activity){
+        my_activity = activity;
+        numberRecoResults = DEFAULT_NUMBER_RESULTS;
+        languageModel = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
+    }
+
+
+    public int getRequestCode(){
+        return REQUEST_CODE;
+    }
+
+
+    public void launchActivity(){
+
+        // Check we have permission to record audio
+        checkASRPermission();
+
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify language model
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, languageModel);
+
+        // Specify mx number of recognition results
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, numberRecoResults);
+
+        // Start listening
+        my_activity.startActivityForResult(intent, REQUEST_CODE);
+    }
+
+
+    /**
+     * Checks whether the user has granted permission to the microphone. If the permission has not been provided,
+     * it is requested. The result of the request (whether the user finally grants the permission or not)
+     * is processed in the onRequestPermissionsResult method.
+     *
+     * This is necessary from Android 6 (API level 23), in which users grant permissions to apps
+     * while the app is running. In previous versions, the permissions were granted when installing the app
+     * See: http://developer.android.com/intl/es/training/permissions/requesting.html
+     */
+    public void checkASRPermission() {
+        if (ContextCompat.checkSelfPermission(my_activity.getApplicationContext(), Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // If  an explanation is required, show it
+            if (ActivityCompat.shouldShowRequestPermissionRationale(my_activity, Manifest.permission.RECORD_AUDIO))
+                Toast.makeText(my_activity.getApplicationContext(), R.string.asr_permission, Toast.LENGTH_SHORT).show();
+
+            // Request the permission.
+            ActivityCompat.requestPermissions(my_activity, new String[]{Manifest.permission.RECORD_AUDIO},
+                    MY_PERMISSIONS_REQUEST_RECORD_AUDIO); //Callback in "onRequestPermissionResult"
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Pair<ArrayList<String>, float[]> onActivityResult(int resultCode, Intent data){
+        if (resultCode == RESULT_OK) {
+            if (data != null) {
+                //Retrieves the N-best list and the confidences from the ASR result
+                ArrayList<String> n_best_list = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                float[] n_best_confidences = data.getFloatArrayExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
+
+                return new Pair<ArrayList<String>, float[]>(n_best_list, n_best_confidences);
+            }
+        }
+
+        return null;
+    }
+}
--- app/src/main/java/com/example/bruno/museomatematico/MainActivity.java
+++ app/src/main/java/com/example/bruno/museomatematico/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.bruno.museomatematico;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,18 +10,13 @@ import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -34,19 +28,11 @@ import java.util.List;
  * status bar and navigation/system bar) with user interaction.
  */
 public class MainActivity extends AppCompatActivity {
+    private TTS mytts;
+    private ASR myasr;
 
-    // Default values for the language model and maximum number of recognition results
-    // They are shown in the GUI when the app starts, and they are used when the user selection is not valid
-    private final static int DEFAULT_NUMBER_RESULTS = 10;
-    private final static String DEFAULT_LANG_MODEL = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
+    private final static String LOGTAG = "MainActivity";
 
-
-    private int numberRecoResults = DEFAULT_NUMBER_RESULTS;
-    private String languageModel = DEFAULT_LANG_MODEL;
-
-    private final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 22;
-    private final static String LOGTAG = "SIMPLEASR";
-    private final static int ASR_CODE = 123;
 
     /**
      * Whether or not the system UI should be auto-hidden after
@@ -84,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         }
     };
-    private View mControlsView;
     private final Runnable mShowPart2Runnable = new Runnable() {
         @Override
         public void run() {
@@ -93,7 +78,6 @@ public class MainActivity extends AppCompatActivity {
             if (actionBar != null) {
                 actionBar.show();
             }
-            mControlsView.setVisibility(View.VISIBLE);
         }
     };
     private boolean mVisible;
@@ -127,6 +111,9 @@ public class MainActivity extends AppCompatActivity {
         mVisible = true;
         mContentView = findViewById(R.id.response_text_view);
 
+        mytts = new TTS(this);
+        myasr = new ASR(this);
+
         setSpeakActionButton();
 
 
@@ -137,59 +124,6 @@ public class MainActivity extends AppCompatActivity {
                 toggle();
             }
         });
-    }
-
-
-    private void listen()  {
-
-        //Disable button so that ASR is not launched until the previous recognition result is achieved
-        FloatingActionButton speak = (FloatingActionButton) findViewById(R.id.speak_action_button);
-        speak.setEnabled(false);
-
-        // Check we have permission to record audio
-        checkASRPermission();
-
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-
-        // Specify language model
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, languageModel);
-
-        // Specify mx number of recognition results
-        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, numberRecoResults);
-
-        // Start listening
-        startActivityForResult(intent, ASR_CODE);
-
-    }
-
-    /**
-     * Checks whether the user has granted permission to the microphone. If the permission has not been provided,
-     * it is requested. The result of the request (whether the user finally grants the permission or not)
-     * is processed in the onRequestPermissionsResult method.
-     *
-     * This is necessary from Android 6 (API level 23), in which users grant permissions to apps
-     * while the app is running. In previous versions, the permissions were granted when installing the app
-     * See: http://developer.android.com/intl/es/training/permissions/requesting.html
-     */
-    public void checkASRPermission() {
-        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // If  an explanation is required, show it
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO))
-                Toast.makeText(getApplicationContext(), R.string.asr_permission, Toast.LENGTH_SHORT).show();
-
-            // Request the permission.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
-                    MY_PERMISSIONS_REQUEST_RECORD_AUDIO); //Callback in "onRequestPermissionResult"
-        }
-    }
-
-
-    private void setRecognitionParams()  {
-
-        numberRecoResults = DEFAULT_NUMBER_RESULTS;
-        languageModel = RecognizerIntent.LANGUAGE_MODEL_FREE_FORM;
     }
 
 
@@ -219,8 +153,10 @@ public class MainActivity extends AppCompatActivity {
                 // find out whether speech recognition is supported
                 List<ResolveInfo> intActivities = packM.queryIntentActivities(new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
                 if (intActivities.size() != 0) {
-                    setRecognitionParams(); //Read speech recognition parameters from GUI
-                    listen();                //Set up the recognizer with the parameters and start listening
+                    //Disable button so that ASR is not launched until the previous recognition result is achieved
+                    FloatingActionButton speak = (FloatingActionButton) findViewById(R.id.speak_action_button);
+                    speak.setEnabled(false);
+                    myasr.launchActivity();                //Set up the recognizer with the parameters and start listening
                 }
                 else
                 {
@@ -236,39 +172,21 @@ public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == ASR_CODE)  {
-            if (resultCode == RESULT_OK)  {
-                if(data!=null) {
-                    //Retrieves the N-best list and the confidences from the ASR result
-                    ArrayList<String> nBestList = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    float[] nBestConfidences = data.getFloatArrayExtra(RecognizerIntent.EXTRA_CONFIDENCE_SCORES);
-
-                    //Creates a collection of strings, each one with a recognition result and its confidence
-                    //following the structure "Phrase matched (conf: 0.5)"
-                    ArrayList<String> nBestView = new ArrayList<>();
-
-                    for(int i=0; i<nBestList.size(); i++) {
-                        if (nBestConfidences != null) {
-                            if (nBestConfidences[i] >= 0)
-                                nBestView.add(nBestList.get(i) + " (conf: " + String.format(this.getResources().getConfiguration().getLocales().get(0), "%.2f", nBestConfidences[i]) + ")");
-                            else
-                                nBestView.add(nBestList.get(i) + " (no confidence value available)");
-                        } else
-                            nBestView.add(nBestList.get(i) + " (no confidence value available)");
-                    }
-
-                    Log.i(LOGTAG, "There were : "+ nBestView.size()+" recognition results");
-                }
-            }
-            else {
-                //Reports error in recognition error in log
-                Log.e(LOGTAG, "Recognition was not successful");
+        if (requestCode == myasr.getRequestCode())  {
+            Pair<ArrayList<String>, float[]> results = myasr.onActivityResult(resultCode, data);
+            if (results != null) {
+                ArrayList<String> n_best_list = results.first;
+                float[] n_best_confidences = results.second;
+                Log.i(LOGTAG, "There were : " + n_best_list.size() + " recognition results");
             }
 
-            //Enable button
-            Button speak = (Button) findViewById(R.id.speak_action_button);
-            speak.setEnabled(true);
+        } else if (requestCode == mytts.getRequestCode()) {
+            mytts.onActivityResult(resultCode, data);
         }
+
+        //Enable button
+        FloatingActionButton speak = (FloatingActionButton) findViewById(R.id.speak_action_button);
+        speak.setEnabled(true);
     }
 
 

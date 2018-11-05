c7e8db282aa7e36f45ceb7c729fc838196ffeac0
InakiMadi <inakimadi@gmail.com>
2018-11-05 19:54:29 +0100

    well matemati2

--- app/src/main/java/com/example/bruno/museomatematico/MatematiBotActivity.java
+++ app/src/main/java/com/example/bruno/museomatematico/MatematiBotActivity.java
@@ -1,210 +1,98 @@
 package com.example.bruno.museomatematico;
 
-import android.os.Bundle;
 import android.Manifest;
-import android.view.View;
-import ai.api.AIDataService;
-import ai.api.AIListener;
-import ai.api.AIServiceException;
-import ai.api.android.AIConfiguration;
-import ai.api.android.AIService;
-import ai.api.model.AIError;
-import ai.api.model.AIRequest;
-import ai.api.model.AIResponse;
-import ai.api.model.Result;
+import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 
 import java.util.HashMap;
+import java.util.Map;
+
+import ai.api.AIListener;
+import ai.api.android.AIConfiguration;
+import ai.api.android.AIService;
+import ai.api.model.AIError;
+import ai.api.model.AIResponse;
+import ai.api.model.Metadata;
+import ai.api.model.Result;
 
 public class MatematiBotActivity extends AppCompatActivity implements AIListener {
     private AIService aiService;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},1);
+        t= (TextView) findViewById(R.id.textView);
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        
+        if(permission != PackageManager.PERMISSION_GRANTED){
+          makeRequest();
+        }
 
         final AIConfiguration config = new AIConfiguration(
                 "101096c066bd400cb4ff31bb3f723b53",
                 AIConfiguration.SupportedLanguages.Spanish,
                 AIConfiguration.RecognitionEngine.System);
 
-        AIService aiService = AIService.getService(this, config);
+        aiService = AIService.getService(this, config);
         aiService.setListener(this);
-
-        final AIDataService aiDataService = new AIDataService(config);
-
-        final AIRequest aiRequest = new AIRequest();
-
-        addBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String message = editText.getText().toString().trim();
-
-                if (!message.equals("")) {
-
-                    ChatMessage chatMessage = new ChatMessage(message, "user");
-                    ref.child("chat").push().setValue(chatMessage);
-
-                    aiRequest.setQuery(message);
-                    new AsyncTask<AIRequest,Void,AIResponse>(){
-
-                        @Override
-                        protected AIResponse doInBackground(AIRequest... aiRequests) {
-                            final AIRequest request = aiRequests[0];
-                            try {
-                                final AIResponse response = aiDataService.request(aiRequest);
-                                return response;
-                            } catch (AIServiceException e) {
-                            }
-                            return null;
-                        }
-                        @Override
-                        protected void onPostExecute(AIResponse response) {
-                            if (response != null) {
-
-                                Result result = response.getResult();
-                                String reply = result.getFulfillment().getSpeech();
-                                ChatMessage chatMessage = new ChatMessage(reply, "bot");
-                                ref.child("chat").push().setValue(chatMessage);
-                            }
-                        }
-                    }.execute(aiRequest);
-                }
-                else {
-                    aiService.startListening();
-                }
-
-                editText.setText("");
-
-            }
-        });
-
-
-
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                ImageView fab_img = (ImageView)findViewById(R.id.fab_img);
-                Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.ic_send_white_24dp);
-                Bitmap img1 = BitmapFactory.decodeResource(getResources(),R.drawable.ic_mic_white_24dp);
-
-
-                if (s.toString().trim().length()!=0 && flagFab){
-                    ImageViewAnimatedChange(MainActivity.this,fab_img,img);
-                    flagFab=false;
-
-                }
-                else if (s.toString().trim().length()==0){
-                    ImageViewAnimatedChange(MainActivity.this,fab_img,img1);
-                    flagFab=true;
-
-                }
-
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-
-        adapter = new FirebaseRecyclerAdapter<ChatMessage, chat_rec>(ChatMessage.class,R.layout.msglist,chat_rec.class,ref.child("chat")) {
-            @Override
-            protected void populateViewHolder(chat_rec viewHolder, ChatMessage model, int position) {
-
-                if (model.getMsgUser().equals("user")) {
-
-
-                    viewHolder.rightText.setText(model.getMsgText());
-
-                    viewHolder.rightText.setVisibility(View.VISIBLE);
-                    viewHolder.leftText.setVisibility(View.GONE);
-                }
-                else {
-                    viewHolder.leftText.setText(model.getMsgText());
-
-                    viewHolder.rightText.setVisibility(View.GONE);
-                    viewHolder.leftText.setVisibility(View.VISIBLE);
-                }
-            }
-        };
-
-        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
-            @Override
-            public void onItemRangeInserted(int positionStart, int itemCount) {
-                super.onItemRangeInserted(positionStart, itemCount);
-
-                int msgCount = adapter.getItemCount();
-                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
-
-                if (lastVisiblePosition == -1 ||
-                        (positionStart >= (msgCount - 1) &&
-                                lastVisiblePosition == (positionStart - 1))) {
-                    recyclerView.scrollToPosition(positionStart);
-
-                }
-
-            }
-        });
-
-        recyclerView.setAdapter(adapter);
-
-
     }
-    public void ImageViewAnimatedChange(Context c, final ImageView v, final Bitmap new_image) {
-        final Animation anim_out = AnimationUtils.loadAnimation(c, R.anim.zoom_out);
-        final Animation anim_in  = AnimationUtils.loadAnimation(c, R.anim.zoom_in);
-        anim_out.setAnimationListener(new Animation.AnimationListener()
-        {
-            @Override public void onAnimationStart(Animation animation) {}
-            @Override public void onAnimationRepeat(Animation animation) {}
-            @Override public void onAnimationEnd(Animation animation)
-            {
-                v.setImageBitmap(new_image);
-                anim_in.setAnimationListener(new Animation.AnimationListener() {
-                    @Override public void onAnimationStart(Animation animation) {}
-                    @Override public void onAnimationRepeat(Animation animation) {}
-                    @Override public void onAnimationEnd(Animation animation) {}
-                });
-                v.startAnimation(anim_in);
-            }
-        });
-        v.startAnimation(anim_out);
+    
+    protected void makeRequest(){
+      ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},101);
+    }
+    
+    @Override
+    public void onRequestPermissionResult(int requestCode, String permissions[], int[] grantResults){
+      switch(requestCode){
+        case 101:{
+          if(grantResults.length == 0
+            || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+          
+          }
+          else{
+            
+          }
+          return;
+        }
+      }
+    }
+    
+    public void botonClickado(View view){
+       aiService.startListening();
     }
 
     @Override
-    public void onResult(ai.api.model.AIResponse response) {
-
-
-        Result result = response.getResult();
-
-        String message = result.getResolvedQuery();
-        ChatMessage chatMessage0 = new ChatMessage(message, "user");
-        ref.child("chat").push().setValue(chatMessage0);
-
-
-        String reply = result.getFulfillment().getSpeech();
-        ChatMessage chatMessage = new ChatMessage(reply, "bot");
-        ref.child("chat").push().setValue(chatMessage);
-
-
+    public void onResult(AIResponse response) {
+        final Result result = response.getResult();
+        final Metadata metadata = result.getMetadata();
+        if (metadata != null) {
+            Log.i("i", "Intent id: " + metadata.getIntentId());
+            Log.i("i", "Intent name: " + metadata.getIntentName());
+        }
+        
+        final HashMap<String, JsonElement> params = result.getParameters();
+        if (params != null && !params.isEmpty()) {
+            Log.i("p", "Parameters: ");
+            for (final Map.Entry<String, JsonElement> entry : params.entrySet()) {
+                Log.i("p", String.format("%s: %s", entry.getKey(), entry.getValue().toString()));
+            }
+        }
+        
+        t.setText("Query: " + result.getResolvedQuery() + "- Action: " + result.getAction());
     }
 
     @Override
-    public void onError(ai.api.model.AIError error) {
+    public void onError(AIError error) {
 
     }
 
